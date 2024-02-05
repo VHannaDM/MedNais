1af4749850a6e5a1bd2da8522663c9ecf0c6d3f6
Hanna Vaitsikhouskaya <hanna.vaitsikhouskaya@gmail.com>
2024-02-05 17:52:35 +0100

    Initial Commit

---
+++ mednais Watch App/mednaisApp.swift
@@ -0,0 +1,17 @@
+//
+//  mednaisApp.swift
+//  mednais Watch App
+//
+//  Created by VAITSIKHOUSKAYA on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct mednais_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mednais/mednaisApp.swift
@@ -0,0 +1,17 @@
+//
+//  mednaisApp.swift
+//  mednais
+//
+//  Created by VAITSIKHOUSKAYA on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct mednaisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
